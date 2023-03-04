1485033dd55703925b57f66bca55c5349278026c
OmniaAhmed208 <oa66494@gmail.com>
2023-03-04 03:39:46 +0200

    create posts

--- database/migrations/2023_02_10_155030_create_media_table.php
+++ database/migrations/2023_02_10_155030_create_media_table.php
@@ -16,14 +16,18 @@ return new class extends Migration
         Schema::create('media', function (Blueprint $table) {
             $table->id();
             // $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-            $table->unsignedBigInteger('post_id')->nullable();
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            // $table->unsignedBigInteger('post_id')->nullable();
+            // $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             // $table->enum('content_type', ['photo', 'video']);
             $table->string('content_type',500);
-
             $table->string('content_path', 500);
             $table->text('alt_text')->nullable();
+            $table->text('caption')->nullable();
+
             $table->timestamps();
         });
     }
